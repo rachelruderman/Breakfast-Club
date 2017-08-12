1750157c7eeea922c91f2ad41742b73abb230475
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-11 19:02:28 -0700

    set up folders for reducers

--- bc-react/src/reducers/index.js
+++ bc-react/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import EventsReducer from './reducer_events'
 
 const rootReducer = combineReducers({
-  events: EventsReducer
+  events: EventsReducer;
 })
 
 export default rootReducer
